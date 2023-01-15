a2aa0da09410c0bb0cf7ff31ba96a26dea473257
Ricardo Cortés <psic.ricardocortes@gmail.com>
2023-01-15 12:59:52 -0600

    Versión 1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char letra;
+        letra = sc.nextLine().charAt(0);
+        System.out.println();
+        if (Character.isLowerCase(letra)); // letra >= 65 && letra <= 90
+
+    }
+}
